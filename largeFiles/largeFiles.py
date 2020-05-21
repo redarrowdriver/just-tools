e4cf78653c167610af8314797f4047b17f1d267d
redarrowdriver <aaron@onedaynerd.com>
2020-05-20 22:56:41 -0400

    Initial commit of largeFiles.py

---
+++ largeFiles/largeFiles.py
@@ -0,0 +1,3 @@
+# Large files will read a directory from the top down and output files that are over or equal
+# to a set file size. This is useful in auditing and scoping clients' servers for backup.
+
